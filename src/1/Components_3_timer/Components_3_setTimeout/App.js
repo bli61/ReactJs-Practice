1d2c14d2cf3da1f51c367d4daf4fa820d25a0189
yipeng945 <wangyi@oregonstate.edu>
2018-08-16 22:13:05 -0700

    SetTimeout
    Using setTimeout to implement timer

---
+++ src/1/Components_3_timer/Components_3_setTimeout/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { number: 0, timestarted: false };
+    }
+
+    addOne = () => {
+        this.setState({ number: this.state.number + 1 });
+    };
+
+    startTime = () => {
+        this.addOne();
+        this.var = setTimeout(this.startTime, 1000);
+
+        this.setState({ timestarted: true });
+    };
+    stopTime = () => {
+        clearTimeout(this.var);
+        this.setState({ timestarted: false });
+    };
+    resetTime = () => {
+        this.setState({ number: 0 });
+    };
+    render() {
+        return (
+            <div>
+                <div className="center">
+                    <h1>{this.state.number}</h1>
+                </div>
+                <div className="App-footer">
+                    <button
+                        className="btn"
+                        onClick={
+                            this.state.timestarted
+                                ? this.stopTime
+                                : this.startTime
+                        }
+                    >
+                        {this.state.timestarted ? "Stop" : "Start"}
+                    </button>
+                    <button className="btn" onClick={this.resetTime}>
+                        Reset
+                    </button>
+                </div>
+            </div>
+        );
+    }
+}
+export default App;
